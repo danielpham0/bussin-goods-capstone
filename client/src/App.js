298f769199942270214a1003dcc3f9e1e7b6affb
ericnguyenle <60944834+ericnguyenle@users.noreply.github.com>
2022-04-10 21:11:55 -0700

    NavBar made

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import CheckoutForm from './components/CheckoutForm'
 import SignUpForm from './components/SignUpForm';
 import LoginForm from './components/LoginForm';
+import Navbar from './components/NavBar';
 
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
@@ -17,11 +19,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Bussin Goods</h1>
         </header>
-        <br/>
         <SignUpForm/>
         <br/>
         <LoginForm/>
@@ -30,6 +32,7 @@ class App extends Component {
           <CheckoutForm/>
         </Elements>
       </div>
+
     );
   }
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './Styles.css'
+import React from 'react'
 
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#Products">Products</a>
-  <a href="#Startups">Startups</a>
-
-</div>
+const Navbar = () => {
+  return (
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-transparent">
+      <br/>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Startups</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  )
+}
+export default Navbar
