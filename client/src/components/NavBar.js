9ab2205c5fccb4cda9dd8a1c26d78790613134db
ericnguyenle <60944834+ericnguyenle@users.noreply.github.com>
2022-04-10 19:39:43 -0700

    nav bar html coded

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Styles.css'
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#Products">Products</a>
+  <a href="#Startups">Startups</a>
+
+</div>
