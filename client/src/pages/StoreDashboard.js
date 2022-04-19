1ed4504d9f2fe367760aae79f750a685875a7b3f
Daniel Pham <danielpham0@gmail.com>
2022-04-19 16:19:31 -0700

    quick fix for updating stores

--- client/src/pages/StoreDashboard.js
+++ client/src/pages/StoreDashboard.js
@@ -1,25 +1,26 @@
 import {React, useState, useEffect, useContext} from 'react';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route, useRouteMatch, useHistory } from 'react-router-dom';
 import StoreSetupForm from '../components/store-dashboard/StoreSetupForm'
 import Dashboard from '../components/store-dashboard/Dashboard';
 import StoreConsole from '../components/store-dashboard/StoreConsole';
 import { UserContext } from '../App';
 
 const StoreDashboard = () =>{
+  const history = useHistory()
   const {user} = useContext(UserContext)
   if (!user || user.account_type != 'Store Owner') {
     history.push("/")
   }
 
   const [stores, setStores] = useState([])
+  async function fetchStores() {
+    let response = await fetch(`http://localhost:3001/api/v1/store/getUserStores`,
+        {method: "GET", credentials: 'include'})
+    let responseJSON = await response.json()
+    setStores(responseJSON)
+    console.log(responseJSON)
+  }
   useEffect(() => {
-        async function fetchStores() {
-            let response = await fetch(`http://localhost:3001/api/v1/store/getUserStores`,
-                {method: "GET", credentials: 'include'})
-            let responseJSON = await response.json()
-            setStores(responseJSON)
-            console.log(responseJSON)
-        }
         fetchStores()
     }, [])
   const { path } = useRouteMatch();
@@ -30,7 +31,9 @@ const StoreDashboard = () =>{
         <Route exact path={`${path}/`}>
           <Dashboard stores={stores}/>
         </Route>
-        <Route path={`${path}/StoreSetup`} component={StoreSetupForm} />
+        <Route path={`${path}/StoreSetup`}>
+          <StoreSetupForm updateStores={fetchStores}/>
+        </Route>
         <Route path={`${path}/:storeID`}>
           <StoreConsole stores={stores}/>
         </Route>
