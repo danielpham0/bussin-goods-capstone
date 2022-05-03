270a45cc775f8851b4df99c10bb4c7c1382d5568
Tommy Lam <tommylam2uw.edu>
2022-05-03 10:58:18 -0700

    added product card component

--- client/src/components/product/ProductCats.js
+++ client/src/components/product/ProductCats.js
@@ -3,7 +3,7 @@ import "./ProductCats.css";
 import { STORE_TYPES } from '../../constants/constants.js';
 import { Link, Route, useRouteMatch } from "react-router-dom";
 import { Component } from 'react';
-
+import ProductCard from './ProductCard.js';
 export class ProductCats extends React.Component {
 
     state = {
@@ -69,71 +69,11 @@ export class ProductCats extends React.Component {
                         })}
                     </div>
 
-                    {this.state.show && <Results type={this.state.type} cards={this.state.obj} />}
+                    {this.state.show && <ProductCard type={this.state.type} cards={this.state.obj} />}
 
                 </div>
 
 
-            </div>
-
-        )
-    }
-}
-
-class Results extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    state = {
-        card: [],
-        type: ''
-    }
-
-    componentDidMount() {
-
-        const url = "/api/v1/store/getAllPublicStores"
-
-        fetch(url)
-            .then(response => response.json())
-            .then(data => this.setState({ card: data }))
-    }
-
-    render() {
-        return (
-
-
-            <div className="row product-cats">
-
-                <h2> Showing results for: {this.props.type}</h2>
-                {this.props.cards.filter(e => e.type == this.props.type).length == 0 &&
-
-                    <p>Sorry, no results were found :/</p>
-
-
-                }
-                {this.props.cards.filter(e => e.type == this.props.type).map((object) => {
-
-                    return (
-                        <div className='col-md-3'>
-                            <div className='card border-secondary'>
-                                <Link to={`/Product/${object._id}`}>
-
-                                <img className='img-fluid card-img-top' src={object.pictures[0]}/>
-                                    <div className='card-body'>
-                                        <h5 className='card-title'>{object.name} <p className="card-price" style={{ textAlign: 'right' }}>${object.cost}</p></h5>
-                                        <h6 className="card-store text-muted">{object.store.name}</h6>
-                                        <p>{object.general_description}</p>
-                                        <p className="card-info" style={{ display: 'inline-block' }}>{object.tagline}</p>
-
-                                    </div>
-                                </Link>
-                            </div>
-
-                        </div>
-
-                    )
-                })}
             </div>
 
         )
